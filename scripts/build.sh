4a4f9fe5449d82b1b4d50fae14f0a9eefd8145fd
Piers Roberts <piersroberts@gmail.com>
2026-01-12 19:55:47 +0000

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "Copying static files..."
-
-# Copy schema to root of site
 mkdir -p site
-cp schemas/v1/retropak.schema.json site/retropak.schema.json
 
-# Copy locale files
-mkdir -p site/locales
-cp locales/en.json site/locales/en.json
-
-echo "Building documentation with mkdocs..."
 mkdocs build
+
+mkdir -p site/schemas/v1
+cp schemas/v1/retropak.schema.json site/schemas/v1/retropak.schema.json
 
 echo "Build complete!"
