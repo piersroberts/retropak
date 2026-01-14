f8ff229317fddc393b1c21ed600ed8a4119a38cf
Piers Roberts <piersroberts@gmail.com>
2026-01-14 19:30:19 +0000

    Refactored repo

---
+++ scripts/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+rm -rf packages/swift/Sources/RetropakSchema/schemas
+rm -rf packages/swift/Sources/RetropakSchema/locales
---
+++ scripts/sync.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+cp -R packages/schema/schemas packages/swift/Sources/RetropakSchema
+cp -R packages/schema/locales packages/swift/Sources/RetropakSchema
