82dd223f7ceb3fd4cf7e35343b2b8d135bf7d728
Piers Roberts <piersroberts@gmail.com>
2026-01-14 21:11:58 +0000

    Simplified schema build

--- scripts/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-rm -rf packages/swift/Sources/RetropakSchema/schemas
-rm -rf packages/swift/Sources/RetropakSchema/locales
--- scripts/sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-cp -R packages/schema/schemas packages/swift/Sources/RetropakSchema
-cp -R packages/schema/locales packages/swift/Sources/RetropakSchema
